414eb828b32f85b9b0954c3703433ce0f908ef9d
Ravi Rajput <rajputravi2070@gmail.com>
2026-01-05 13:17:43 +0530

    refactor: simplify import statements in AuthController and clean up pom.xml formatting

--- Ecom/src/main/java/com/vinit/Ecom/controller/AuthController.java
+++ Ecom/src/main/java/com/vinit/Ecom/controller/AuthController.java
@@ -4,10 +4,7 @@ import com.vinit.Ecom.dto.SignInDTO;
 import com.vinit.Ecom.dto.SignUpDTO;
 import com.vinit.Ecom.service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
